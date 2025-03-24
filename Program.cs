2710fbbf9851451d2b6d5bb58fc5bf46d200561b
Yasmim de Castro <Yas.castr0.r@gmail.com>
2025-03-24 10:32:51 -0300

    Finaliza Programa

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Digite uma frase:");
+string frase = Console.ReadLine()!;
+string fraseModificada = frase
+                    .Replace("r", "l")
+                    .Replace("R", "L");
+
+Console.WriteLine($"{fraseModificada}");
